be6ab1eda99ed164b8e3fb218f9532b2d3e8d066
isaiah <isaiahwkahler@gmail.com>
2017-01-30 18:04:10 -0500

    Continued work, adding gyro correction to driving.

--- src/org/usfirst/frc/team5263/robot/AutoVirtualDriver.java
+++ src/org/usfirst/frc/team5263/robot/AutoVirtualDriver.java
@@ -40,7 +40,9 @@ public class AutoVirtualDriver {
 	double driveBeenDone;
 	double power;
 	boolean doRotate = false;
-	double Kp = 0.03;
+	double currentAngle;
+	double drivingMin;
+	double drivingMax;
 
 	// ================================================
 
@@ -72,15 +74,7 @@ public class AutoVirtualDriver {
 	public void init(String mode) {
 		angleOffset = sensing.getGyroAngle();
 		encoder1Val = sensing.getEncoder1();
-		// firstAngle = angleOffset;
-		// angleOffset = sensing.getGyroAngle();
-		// angle = angleOffset - firstAngle;
-		/**
-		 * System.out.println("steps" + steps); turn[steps] = 90;
-		 * distance[steps] = 50; steps ++; turn[steps] = 180; distance[steps] =
-		 * 30; steps++; turn[steps] = 45; distance[steps] = 100;
-		 **/
-
+		sensing.gyro.calibrate();
 	}
 
 	public void periodicAuto() {
@@ -121,151 +115,11 @@ public class AutoVirtualDriver {
 				}
 			}
 		}
-		System.out.println("program terminated.");
+		//System.out.println("program terminated.");
 
 	}
 
-	// if ()) {
-
-	// } else {
-	// manipulators.myRobot.tankDrive(0, 0);
-	// step ++;
-	// }
-
-	/** 
-myRobot.drive(-1.0, -angle*Kp);
-	 * 
-	 * switch (autoRunner){
-	 * 
-	 * case 1:
-	 * 
-	 * 
-	 * 
-	 * 
-	 * encoder1Val = sensing.getEncoder1(); angleOffset =
-	 * sensing.getGyroAngle(); angle = angleOffset - firstAngle;
-	 * System.out.println("angle Offset: " + angleOffset);
-	 * System.out.println("first angle: " + firstAngle); rep = rep + 1;
-	 * System.out.println("counter: " + rep);
-	 * 
-	 * 
-	 * if(rep == 1){ angle = angle - angleOffset; } System.out.println(step);
-	 * autoRunner = 2; break;
-	 * 
-	 * 
-	 * 
-	 * case 2: encoder1Val = sensing.getEncoder1(); angleOffset =
-	 * sensing.getGyroAngle(); angle = angleOffset - firstAngle; double
-	 * minMargin = turn[step] - 5; double maxMargin = turn[step] + 5;
-	 * 
-	 * if(angle < minMargin){
-	 * 
-	 * if(minMargin - angle > 20){ manipulators.myRobot.tankDrive(0.6, -0.6);
-	 * System.out.println("fast min"); }else{
-	 * manipulators.myRobot.tankDrive(0.375, -0.375); System.out.println("slow
-	 * min"); } System.out.println("angle " + angle + " less than specified " +
-	 * turn[step]);
-	 * 
-	 * }else if(angle > maxMargin){
-	 * 
-	 * if(angle - maxMargin > 20){ manipulators.myRobot.tankDrive(-0.6, 0.6);
-	 * System.out.println("fast max"); }else {
-	 * manipulators.myRobot.tankDrive(-0.375, -0.375); System.out.println("slow
-	 * max"); } System.out.println("angle " + angle + " more than specified " +
-	 * turn[step]);
-	 * 
-	 * }else{ autoRunner = 3; }
-	 * 
-	 * break;
-	 * 
-	 * 
-	 * case 3: encoder1Val = sensing.getEncoder1(); System.out.println("running
-	 * drive"); System.out.println("step " + step + " steps " + steps);
-	 * encoderSet = encoderSet + 1; if(encoderSet == 1){
-	 * sensing.encoder1.reset(); encoderMin = distance[step] - 50; encoderMax =
-	 * distance[step] + 50; } if(encoder1Val < encoderMin){
-	 * System.out.println("encoder val " + encoder1Val + " less than " +
-	 * distance[step]); manipulators.myRobot.tankDrive(0.4, 0.4); }else
-	 * if(encoder1Val > encoderMax){ System.out.println("encoder val " +
-	 * encoder1Val + " more than " + distance[step]);
-	 * manipulators.myRobot.tankDrive(-0.4, -0.4); }else{ if(step < steps - 1){
-	 * //whats happening is that this is repeated over and over and always
-	 * defaults to the else, and the condition is true step = step + 1;
-	 * System.out.println("STEP HIGHER"); autoRunner = 4;
-	 * 
-	 * } }
-	 * 
-	 * 
-	 * 
-	 * break; case 4: autoRunner = 1; break; default: break; }
-	 * 
-	 * 
-	 * 
-	 * 
-	 * 
-	 * 
-	 **/
-
-	/**
-	 * 
-	 * double encoder1Val = sensing.getEncoder1(); angleOffset =
-	 * sensing.getGyroAngle(); System.out.println("angle Offset: " +
-	 * angleOffset); System.out.println("first angle: " + firstAngle); rep = rep
-	 * + 1; System.out.println("counter: " + rep); double angle = angleOffset -
-	 * firstAngle;
-	 * 
-	 * 
-	 * if(rep == 1){ angle = angle - angleOffset; }
-	 **/
-
-	// =======================================================================================
-
-	/**
-	 * if(angle < smallerDegrees && rep < 500){
-	 * manipulators.myRobot.tankDrive(0.6, -0.6); //try this
-	 * System.out.println("angle smaller than specified :" + angle); }else
-	 * if(angle > biggerDegrees && rep < 500){
-	 * manipulators.myRobot.tankDrive(-0.6, 0.6); //maybe change
-	 * System.out.println("angle larger than specified :" + angle ); } else{
-	 * if(rep < 500 && encoder1Val < 100){ manipulators.myRobot.tankDrive(0.4,
-	 * 0.4); System.out.println("driving straght"); }
-	 * 
-	 * }
-	 **/
-
-	// ===================================================================================================
-
-	/**
-	 * 
-	 * double minMargin = turn[step] - 5; double maxMargin = turn[step] + 5;
-	 * 
-	 * if(angle < minMargin){ if(minMargin - angle > 20){
-	 * manipulators.myRobot.tankDrive(0.6, -0.6); System.out.println("fast
-	 * min"); }else { manipulators.myRobot.tankDrive(0.375, -0.375);
-	 * System.out.println("slow min"); }
-	 * 
-	 * System.out.println("angle " + angle + " less than specified " +
-	 * turn[step]); }else if(angle > maxMargin){ if(angle - maxMargin > 20){
-	 * manipulators.myRobot.tankDrive(-0.6, 0.6); System.out.println("fast
-	 * max"); }else { manipulators.myRobot.tankDrive(-0.375, -0.375);
-	 * System.out.println("slow max"); } System.out.println("angle " + angle + "
-	 * more than specified " + turn[step]); }else{ System.out.println("finished
-	 * turn steps"); after = after + 1; if(after == 1){ afterTurn = encoder1Val;
-	 * } System.out.println("encoder val " + encoder1Val);
-	 * System.out.println("AFTER TURN " + afterTurn + " AFTER " + after);
-	 * if(encoder1Val < (distance[step] + afterTurn)){
-	 * System.out.println("encoder val " + encoder1Val + " less than " +
-	 * distance[step] + " plus " + " after turn " + afterTurn + " : " +
-	 * distance[step] + afterTurn); manipulators.myRobot.tankDrive(0.4, 0.4);
-	 * //what i saw was that the encoder val goes up from turning, reset that
-	 * with a variable. }else{ System.out.println("finished turn step"); if(step
-	 * < steps){
-	 * //System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa steps "
-	 * + steps + " step " + step); step ++; } System.out.println("steps
-	 * writtern: " + steps); System.out.println("current step: " + step); } };
-	 * 
-	 * 
-	 **/
+//=================================================================================================
 
 	public boolean DriveStraightN(double driveDistance, double power) {
 
@@ -282,15 +136,17 @@ myRobot.drive(-1.0, -angle*Kp);
 			sensing.gyro.reset();
 			encoderMin = drivePulses - 50;
 			encoderMax = drivePulses + 50;
+			currentAngle = sensing.getGyroAngle();
+			drivingMin = currentAngle - 5;
+			drivingMax = currentAngle + 5;
+			
 		}
 		if (encoder1Val < encoderMin) {
 			System.out.println("encoder val " + encoder1Val + " less than " + drivePulses);
-			//manipulators.myRobot.tankDrive(power, power); //power, power
-			manipulators.myRobot.drive(power, -angle*Kp);
+			manipulators.myRobot.tankDrive(power, power); 
 		} else if (encoder1Val > encoderMax) {
 			System.out.println("encoder val " + encoder1Val + " more than " + drivePulses);
-			//manipulators.myRobot.tankDrive(-power, -power);
-				manipulators.myRobot.drive(-power, -angle*Kp);
+			manipulators.myRobot.tankDrive(-power, -power);
 				
 		} else {
 			System.out.println("between margins, stopped.");
@@ -305,11 +161,10 @@ myRobot.drive(-1.0, -angle*Kp);
 		return true;
 	}
 
+//==================================================
+	
 	public boolean Rotate(double degrees) {
-		// this.degrees = degrees;
-
-		// angleOffset = sensing.getGyroAngle();
-
+		
 		rotateSet++;
 		if (rotateSet == 1) {
 			// firstAngle = angleOffset;
@@ -326,9 +181,11 @@ myRobot.drive(-1.0, -angle*Kp);
 		if (angle < minMargin) {
 			manipulators.myRobot.tankDrive(0.6, -0.6);
 			System.out.println("angle " + angle + " less than specified " + degrees);
+			rotateBeenDone = 0;
 		} else if (angle > maxMargin) {
 			manipulators.myRobot.tankDrive(-0.6, 0.6);
 			System.out.println("angle " + angle + " less than specified " + degrees);
+			rotateBeenDone = 0;
 		} else {
 			manipulators.myRobot.tankDrive(0, 0);
 			rotateBeenDone = rotateBeenDone + 1;
@@ -337,28 +194,8 @@ myRobot.drive(-1.0, -angle*Kp);
 				return false;
 			}
 		}
-
-		/**
-		 * if(angle < minMargin){
-		 * 
-		 * if(minMargin - angle > 20){ manipulators.myRobot.tankDrive(0.6,
-		 * -0.6); //faster, farther System.out.println("fast min"); }else{
-		 * manipulators.myRobot.tankDrive(0.375, -0.375); //slower, closer
-		 * System.out.println("slow min"); } System.out.println("angle " + angle
-		 * + " less than specified " + degrees);
-		 * 
-		 * }else if(angle > maxMargin){
-		 * 
-		 * if(angle - maxMargin > 20){ manipulators.myRobot.tankDrive(-0.6,
-		 * 0.6); System.out.println("fast max"); }else {
-		 * manipulators.myRobot.tankDrive(-0.375, 0.375);
-		 * System.out.println("slow max"); } System.out.println("angle " + angle
-		 * + " more than specified " + degrees);
-		 * 
-		 * }else{ System.out.println("Rotation between margins, stopping.");
-		 * rotateBeenDone = rotateBeenDone + 1; if(rotateBeenDone > 1000){ //add
-		 * return thing return false; } }
-		 **/
+		
+		
 		return true;
 
 	}
--- src/org/usfirst/frc/team5263/robot/TeleOperated.java
+++ src/org/usfirst/frc/team5263/robot/TeleOperated.java
@@ -1,14 +1,39 @@
 package org.usfirst.frc.team5263.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
+
 public class TeleOperated {
 
+	Joystick main;
+	Joystick log1;
+	Joystick log2;
+	double left_pow;
+	double right_pow;
+	Sensing sensing;
+	Manipulators manipulators;
+	
+	
+	
 	public TeleOperated(Sensing sensing, CameraMan cameraMan, CameraMonitor cameraMonitor, Manipulators manipulators) {
+
+
+		this.sensing = sensing;
+		this.manipulators = manipulators;
 		
+		//manipulators.myRobot.tankDrive(, 0.5);
 		
 	}
 	
 	public void init(String mode) {
-		
+		main = new Joystick(0);
+		log1 = new Joystick(0);
+		log2 = new Joystick(2);
 	}
+	
+	//left_pow = main.getRawAxis(1);
+	
+	
+	
+	
 	
 }
